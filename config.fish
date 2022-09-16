8cc0b40c9f13b963c82760c767cfab8cf040b8b2
yuu_harada <yuu_harada@MC201903005.zeus.mcinc.local>
2022-09-16 17:21:18 +0900

    [fish]android関連のパスを追加

--- config.fish
+++ config.fish
@@ -2,4 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# windows_path
+fish_add_path ~/windows_path
+
+# adb
 fish_add_path /mnt/c/Users/yuu_harada/AppData/Local/Android/Sdk/platform-tools
+
+# aapt
+fish_add_path /mnt/c/Users/yuu_harada/AppData/Local/Android/Sdk/build-tools/33.0.0-rc1
