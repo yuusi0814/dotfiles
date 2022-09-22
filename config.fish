bcc40556623805456326f0461e8861f8cb9e8642
yuu_harada <yuu_harada@MC201903005.zeus.mcinc.local>
2022-09-22 10:40:50 +0900

    [fish]fish_add_pathをscoopに変更

--- config.fish
+++ config.fish
@@ -6,7 +6,7 @@ end
 fish_add_path ~/windows_path
 
 # adb
-fish_add_path /mnt/c/Users/yuu_harada/AppData/Local/Android/Sdk/platform-tools
+fish_add_path /mnt/c/Users/yuu_harada/scoop/apps/adb/current/platform-tools
 
 # aapt
-fish_add_path /mnt/c/Users/yuu_harada/AppData/Local/Android/Sdk/build-tools/33.0.0-rc1
+fish_add_path /mnt/c/Users/yuu_harada/scoop/android-sdk/current/build-tools/33.0.0
